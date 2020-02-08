db7a09478e02b8b16097d93ffcef112782a3b1b9
Rajiv <emailrajiv.r@gmail.com>
2020-02-09 01:07:13 +0530

    added google login function

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,152 @@
 import 'package:flutter/material.dart';
-import 'package:platter/screens/getstarted.dart';
-import 'package:platter/screens/home.dart';
-import 'screens/landing.dart';
-import 'screens/login.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:platter/screens/ProfileScreen.dart';
 
-void main() => runApp(Platter());
+void main() => runApp(MyApp());
+bool isSigned = false;
 
-class Platter extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BottomNavBar(),
+      title: 'Flutter Google Signin APP',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff9C58D2),
+      ),
+      home: GoogleSignApp(),
     );
   }
+}
+
+class GoogleSignApp extends StatefulWidget {
+  @override
+  _GoogleSignAppState createState() => _GoogleSignAppState();
+}
+
+class _GoogleSignAppState extends State<GoogleSignApp> {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googlSignIn = GoogleSignIn();
+
+  Future<FirebaseUser> _signIn(BuildContext context) async {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('Please sign in'),
+    ));
+
+    final GoogleSignInAccount googleUser = await _googlSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    FirebaseUser userDetails =
+        (await _firebaseAuth.signInWithCredential(credential)).user;
+
+    UserDetails details = UserDetails(
+      userDetails.displayName,
+      userDetails.photoUrl,
+      userDetails.email,
+    );
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ProfileScreen(detailsUser: details),
+      ),
+    );
+    return userDetails;
+  }
+
+  void isSignedin() async {
+    bool isSigned = await _googlSignIn.isSignedIn();
+    FirebaseUser firebaseuser = await _firebaseAuth.currentUser();
+    if (isSigned) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProfileScreen(
+              detailsUser: UserDetails(firebaseuser.displayName,
+                  firebaseuser.photoUrl, firebaseuser.email)),
+        ),
+      );
+    } else {
+      print('signed out');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) => Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Image.network(
+                  'https://images.unsplash.com/photo-1518050947974-4be8c7469f0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                  fit: BoxFit.fill,
+                  color: Color.fromRGBO(255, 255, 255, 0.6),
+                  colorBlendMode: BlendMode.modulate),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                Container(
+                    width: 250.0,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                        color: Color(0xffffffff),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.google,
+                              color: Color(0xffCE107C),
+                            ),
+                            SizedBox(width: 10.0),
+                            Text(
+                              'Sign in with Google',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 18.0),
+                            ),
+                          ],
+                        ),
+                        onPressed: () => _signIn(context)
+                            .then((FirebaseUser user) => print(user))
+                            .catchError((e) => print(e)),
+                      ),
+                    )),
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserDetails {
+  final String userName;
+  final String photoUrl;
+  final String userEmail;
+
+  UserDetails(this.userName, this.photoUrl, this.userEmail);
 }
